9e3931e611bbd55b30e4d5f9b0920ece74e3098e
Alex Ellis <alexellis2@gmail.com>
2018-04-28 16:14:51 +0100

    Update handler.py

--- kubecon/handler.py
+++ kubecon/handler.py
@@ -1,2 +1,6 @@
-def handle(st):
-    return 'You said: "' + st + '"'
+def handle(req):
+    msg = req
+    if len(req) == 0:
+        msg = "pass a value to the function"
+
+    return str.format("You said: {}", msg)
