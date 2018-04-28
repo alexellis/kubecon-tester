0ff6e58d2f2380ea8ee6a42332bee3456e14a628
Alex Ellis <alexellis2@gmail.com>
2018-04-28 20:20:03 +0100

    Add valid hashing unit test
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- bcrypt/handle_test.go
+++ bcrypt/handle_test.go
@@ -2,16 +2,31 @@ package function
 
 import "testing"
 
+const sep = " "
+
 func Test_ValidHash_GivesTrue(t *testing.T) {
-	decoded := decode([]byte("Alex $2a$10$2UEqmAEoGiif4KyOws.5.exi1XfWnpiK2XeQKQum4k9gLLkemJREa"))
+	req := "Alex" + sep + "$2a$10$2UEqmAEoGiif4KyOws.5.exi1XfWnpiK2XeQKQum4k9gLLkemJREa"
+	decoded := decode([]byte(req))
 	if decoded != "true" {
-		t.Errorf("decode, want: %s, got: %s", "true", decoded)
+		t.Errorf("decode (%s), want: %s, got: %s", req, "true", decoded)
 	}
 }
 
 func Test_InvalidHash_GivesFalse(t *testing.T) {
-	decoded := decode([]byte("alex $2a$10$2UEqmAEoGiif4KyOws.5.exi1XfWnpiK2XeQKQum4k9gLLkemJREa"))
+	req := "alex" + sep + "$2a$10$2UEqmAEoGiif4KyOws.5.exi1XfWnpiK2XeQKQum4k9gLLkemJREa"
+	decoded := decode([]byte(req))
 	if decoded != "false" {
-		t.Errorf("decode, want: %s, got: %s", "false", decoded)
+		t.Errorf("decode (%s), want: %s, got: %s", req, "false", decoded)
+	}
+}
+
+func Test_DefaultComplexity_GivesValidHash(t *testing.T) {
+
+	input := "Alex"
+	encoded := Handle([]byte(input))
+	decodedValue := decode([]byte(input + sep + encoded))
+	wantDecoded := "true"
+	if decodedValue != wantDecoded {
+		t.Errorf("decode, want: %s, got: %s", wantDecoded, decodedValue)
 	}
 }
